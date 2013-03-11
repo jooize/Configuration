8fe79b629bb1bcf7a677eb21a8308ebc9695ba79
Joel Esko <joel@yuio.se>
2013-03-11 23:13:25 +0100

    Remove Oh-My-Zsh

--- Install-Dotfiles.sh
+++ Install-Dotfiles.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 git clone git://github.com/jooize/Dotfiles.git $HOME/.dotfiles
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.dotfiles/.oh-my-zsh
 zsh "`dirname $0`"/Install-Helper-Prezto.zsh
 sh "`dirname $0`"/Symlink-Dotfiles.sh
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
