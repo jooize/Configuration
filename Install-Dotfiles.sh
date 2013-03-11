6734301b0a0621576b8812d66241300d53b50575
Joel Esko <joel@yuio.se>
2013-03-11 23:31:06 +0100

    Update scripts

--- Install-Dotfiles.sh
+++ Install-Dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dotfiles="$HOME"/.dotfiles
-git clone git://github.com/jooize/Dotfiles.git "${dotfiles}"/.dotfiles
+git clone git://github.com/jooize/Dotfiles.git "${dotfiles}"
 zsh "${dotfiles}"/Install-Helper-Prezto.zsh
 sh "${dotfiles}"/Symlink-Dotfiles.sh
 git clone https://github.com/gmarik/vundle.git "${dotfiles}"/.vim/bundle/vundle
