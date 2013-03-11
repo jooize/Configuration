45dd1d370392be9b5f491f5842e6350fe1ce83de
Joel Esko <joel@yuio.se>
2013-03-11 22:45:58 +0100

    Switch from Oh-My-Zsh to Prezto

--- Install-Dotfiles.sh
+++ Install-Dotfiles.sh
@@ -2,6 +2,7 @@
 
 git clone git://github.com/jooize/Dotfiles.git $HOME/.dotfiles
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.dotfiles/.oh-my-zsh
+zsh "`dirname $0`"/Install-Helper-Prezto.zsh
 sh "`dirname $0`"/Symlink-Dotfiles.sh
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 vim +BundleInstall +qall
